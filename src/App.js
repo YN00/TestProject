d493eea8bb56bdae3c89cdef2dbf4eb2bae900ea
YN00 <40154658+YN00@users.noreply.github.com>
2020-02-26 16:24:51 +0900

    docker
    docker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
-export default class App extends React.Component {
+class App extends React.Component {
     render() {
         return (
             <div>
@@ -9,3 +10,5 @@ export default class App extends React.Component {
         )
     }
 }
+
+export default App
